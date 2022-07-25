1b8d5de9706fb6c57a798dc00b3f420d0afa4f8f
muhamadp <Mnemezius1991!!>
2022-07-26 01:28:19 +0200

    fix: create html and default value separator

--- src/lib.tsx
+++ src/lib.tsx
@@ -11,13 +11,18 @@ type Props = {
 };
 
 const ReactBionic: React.FC<Props> = ({ sx, className, render, fixationPoint = 1, separator = '<b>' }) => {
+  const defaultSeparator = React.useMemo(
+    () => [separator, separator.split('').splice(1, 0, '/').join('')],
+    [separator]
+  );
+
   const htmlSeparator = React.useMemo(() => {
-    return separator === '**' ? separator : separator.split('').splice(1, 0, '/').join('');
-  }, [separator]);
+    return separator === '**' ? separator : defaultSeparator;
+  }, [defaultSeparator, separator]);
 
   const text2bionic = React.useMemo(
-    () => text.textVide(render, { fixationPoint, sep: htmlSeparator }),
-    [fixationPoint, htmlSeparator, render]
+    () => text.textVide(render, { fixationPoint, sep: separator ? htmlSeparator : defaultSeparator }),
+    [defaultSeparator, fixationPoint, htmlSeparator, render, separator]
   );
 
   const purify = React.useMemo(() => DOMPurify.sanitize(text2bionic), [text2bionic]);
